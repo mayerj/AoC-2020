2f2d23608ee904aa61ef0ebbe85a811b5ba92472
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2020-12-05 01:20:58 -0500

    Day 2 part 2

--- 2/Program.cs
+++ 2/Program.cs
@@ -7,14 +7,8 @@ namespace AoC
 {
     class Program
     {
-        static void Main(string[] args)
+        private static (int n1, int n2, char c, string password) Parse(string entry)
         {
-            System.Console.WriteLine(Input.Data.Count(x => IsValid(x)));
-        }
-
-        private static bool IsValid(string entry)
-        {
-            System.Console.WriteLine(entry);
 
             Regex matcher = new Regex(@"(?<min>\d+)-(?<max>\d+) (?<char>.): (?<password>.+)");
 
@@ -25,7 +19,20 @@ namespace AoC
             string c = match.Groups["char"].Value;
             string password = match.Groups["password"].Value;
 
-            int occurences = password.Count(x => x == c[0]);
+            return (min, max, c[0], password);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Input.Data.Count(x => IsValidPart1(x)));
+            System.Console.WriteLine(Input.Data.Count(x => IsValidPart2(x)));
+        }
+
+        private static bool IsValidPart1(string entry)
+        {
+            (int min, int max, char c, string password) = Parse(entry);
+
+            int occurences = password.Count(x => x == c);
 
             if (occurences >= min && occurences <= max)
             {
@@ -33,7 +40,21 @@ namespace AoC
             }
 
             return false;
+        }
 
+        private static bool IsValidPart2(string entry)
+        {
+            (int pos1, int pos2, char c, string password) = Parse(entry);
+
+            bool isPos1 = password[pos1 - 1] == c;
+            bool isPos2 = password[pos2 - 1] == c;
+
+            if (isPos1 == isPos2)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
