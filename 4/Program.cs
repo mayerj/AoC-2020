b962f2a309cd7bc27b601a4b4f63130cae953983
Jean-Paul Mayer <4032461+mayerj@users.noreply.github.com>
2020-12-05 14:04:45 -0500

    Day 4 part 2

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AoC
 {
@@ -9,30 +10,121 @@ namespace AoC
         {
             SelfTest();
             Part1();
+            Part2();
+        }
+        private static bool Part1Validity(Passport passport)
+        {
+            return new[] { passport.BirthYear, passport.ExpirationYear, passport.EyeColor, passport.HairColor, passport.Height, passport.IssueYear, passport.PassportId }.All(x => !string.IsNullOrWhiteSpace(x));
         }
 
         private static void SelfTest()
         {
-            var passports = PassportParser.Parse(Input.TestData);
+            Run(nameof(SelfTest), Input.TestData, Part1Validity);
+        }
 
-            static bool IsValid(Passport passport)
-            {
-                return new[] { passport.BirthYear, passport.ExpirationYear, passport.EyeColor, passport.HairColor, passport.Height, passport.IssueYear, passport.PassportId }.All(x => !string.IsNullOrWhiteSpace(x));
-            }
+        private static void Run(string part, string input, Func<Passport, bool> isValid)
+        {
+            var passports = PassportParser.Parse(input);
 
-            System.Console.WriteLine(passports.Count(IsValid));
+            System.Console.WriteLine($"{part} {passports.Count(isValid)}");
         }
 
         private static void Part1()
         {
-            var passports = PassportParser.Parse(Input.Data);
+            Run(nameof(Part1), Input.Data, Part1Validity);
+        }
 
-            static bool IsValid(Passport passport)
+        private static void Part2()
+        {
+            Run(nameof(Part2), Input.Data, Part2Validity);
+        }
+
+        private static bool Part2Validity(Passport passport)
+        {
+            if (!Part1Validity(passport)) return false;
+
+            static bool ValidateYear(string year, int minimum, int maximum)
             {
-                return new[] { passport.BirthYear, passport.ExpirationYear, passport.EyeColor, passport.HairColor, passport.Height, passport.IssueYear, passport.PassportId }.All(x => !string.IsNullOrWhiteSpace(x));
+                if (!int.TryParse(year, out var yearNumber))
+                {
+                    return false;
+                }
+
+                return yearNumber >= minimum && yearNumber <= maximum;
             }
 
-            System.Console.WriteLine(passports.Count(IsValid));
+            static bool ValidateHeight(string height, params (string unit, int min, int max)[] options)
+            {
+                Regex regex = new(@"(?<num>\d+).+");
+
+                var match = regex.Match(height);
+                if (!match.Success)
+                {
+                    return false;
+                }
+
+                string numAsString = match.Groups["num"].Value;
+
+                if (!int.TryParse(numAsString, out var num))
+                {
+                    return false;
+                }
+
+                foreach (var (unit, min, max) in options)
+                {
+                    if (!height.EndsWith(unit))
+                    {
+                        continue;
+                    }
+
+                    if (num >= min && num <= max)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+
+                return false;
+            }
+
+            static bool ValidateHairColor(string hairColor)
+            {
+                return hairColor.Length == 7 && hairColor[0] == '#' && hairColor.Skip(1).All(x => (x >= '0' && x <= '9') || (x >= 'a' && x <= 'f'));
+            }
+
+            static bool ValidateEyeColor(string eyeColor)
+            {
+                return eyeColor switch
+                {
+                    "amb" => true,
+                    "blu" => true,
+                    "brn" => true,
+                    "gry" => true,
+                    "grn" => true,
+                    "hzl" => true,
+                    "oth" => true,
+                    _ => false,
+                };
+            }
+
+            static bool ValidatePassportId(string passportId)
+            {
+                return passportId.Length == 9 && passportId.All(x => char.IsNumber(x));
+            }
+
+            bool[] predicates = new[]
+            {
+                ValidateYear(passport.BirthYear, 1920, 2002),
+                ValidateYear(passport.IssueYear, 2010, 2020),
+                ValidateYear(passport.ExpirationYear, 2020, 2030),
+                ValidateHeight(passport.Height, ("cm", 150, 193), ("in", 59, 76)),
+                ValidateHairColor(passport.HairColor),
+                ValidateEyeColor(passport.EyeColor),
+                ValidatePassportId(passport.PassportId),
+            };
+
+            return predicates.All(x => x);
         }
     }
 }
